2efdf2bef64d9c595e21c70f031285d6278712d9
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2022-07-25 17:14:23 -0300

    fim

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade('Daniel', 24));
+console.log(mostraCidade('Daniel', 'DF'));
+console.log(mostraHobby('Daniel', 'Ciclismo'));
